4f6247f23e5d64b6b5b13f7ed0a937433a89d522
javieter <olmedo_javier@hotmail.com>
2020-04-01 16:10:29 -0300

    ejercicio3, aceptable

---
+++ EjerciciosGit/src/ejercicio3Javier/Atleta.java
@@ -0,0 +1,33 @@
+package ejercicio3Javier;
+
+public class Atleta {
+	private String nombre;
+	private double tiempo;
+
+	public Atleta(String nombre, double tiempo) {
+		this.nombre = nombre;
+		this.tiempo = tiempo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(double tiempo) {
+		this.tiempo = tiempo;
+	}
+
+	@Override
+	public String toString() {
+		return "Atleta [nombre=" + nombre + ", tiempo=" + tiempo + "]";
+	}
+
+}
