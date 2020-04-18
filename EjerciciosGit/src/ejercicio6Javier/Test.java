e52afea596117d670c2ee9481105fc4aa71add4d
javieter <olmedo_javier@hotmail.com>
2020-04-18 20:09:08 -0300

    actualizaciones de ejercicios

--- EjerciciosGit/src/ejercicio6Javier/Test.java
+++ EjerciciosGit/src/ejercicio6Javier/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GastoAnual gastoAnual = new GastoAnual();
-		/*gastoAnual.agregarGasto(Mes.ENERO, "administracion", 1500);
+		gastoAnual.agregarGasto(Mes.ENERO, "administracion", 1500);
 		gastoAnual.agregarGasto(Mes.ENERO, "tecnico", 150);
 		gastoAnual.agregarGasto(Mes.FEBRERO, "tecnico", 500);
 		gastoAnual.agregarGasto(Mes.MARZO, "comercial", 750);
@@ -18,23 +18,25 @@ public class Test {
 		gastoAnual.agregarGasto(Mes.OCTUBRE, "comercial", 300);
 		gastoAnual.agregarGasto(Mes.NOVIEMBRE, "administracion", 1000);
 		gastoAnual.agregarGasto(Mes.DICIEMBRE, "comercial", 400);
-		gastoAnual.agregarGasto(Mes.OCTUBRE, "funcional", 7200);*/
-		
+		gastoAnual.agregarGasto(Mes.OCTUBRE, "funcional", 7200);
+
 		System.out.println("Gasto del rubro tecnico: " + gastoAnual.gastoAcumulado("tecnico"));
 		System.out.println("Gasto del rubro administracion: " + gastoAnual.gastoAcumulado("administracion"));
 		System.out.println("Gasto del rubro comercial: " + gastoAnual.gastoAcumulado("comercial"));
 		System.out.println("Gasto del rubro funcional: " + gastoAnual.gastoAcumulado("funcional"));
+
+		System.out.println("\nInformacion de los conusmos por mes");
 		
 		gastoAnual.informarConsumosPorMes();
-		
+
 		System.out.println();
-		
+
 		gastoAnual.informarMesMayorConsumo();
-		
-		System.out.println();
-		
+
+		System.out.println("\n\n"+"Promedio mensual por rubro");
+
 		gastoAnual.informarPromedioMensualPorRubro();
-		
+
 	}
 
 }
